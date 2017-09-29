41ba3ca4aef89a7b5dd94bc3f8b937fc6b7032dd
Akki <akshatuppal_vijay@srmuniv.edu.in>
2017-09-29 16:02:04 +0530

    Update views.py

--- fb_chatbot/views.py
+++ fb_chatbot/views.py
@@ -38,12 +38,12 @@ def post_facebook_message(fbid, received_message):
         joke_text = "I didn't understand! Send 'stupid', 'fat', 'dumb' for a funny joke!"
 
 	user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid
-	user_details_params = {'fields':'first_name, last_name, profile_pic', 'access_token':'EAAUzZB07kpkIBAIeY4EXAi0wTHtXLKvQqtx2fJFGwYYJKy190y52LpMP2EmhsZC0CdFDFNOZBhpGvgu38NjdqOZBg7lp19hvhvVo6YTkNCwL3G2lTJIY4NnWSYWX0SJZCO7XYOO8QL6vZAZCHKV0ZAUgZBMQHfjfTYADXRMK3pKpimWy5tv1Pc5Ea'}
+	user_details_params = {'fields':'first_name, last_name, profile_pic', 'access_token':'<ACCESS_TOKEN>'}
 	user_details = requests.get(user_details_url, user_details_params).json()
 	joke_text = 'Hey ' + user_details['first_name'] + '..! ' + joke_text   
 
 
-    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAUzZB07kpkIBAIeY4EXAi0wTHtXLKvQqtx2fJFGwYYJKy190y52LpMP2EmhsZC0CdFDFNOZBhpGvgu38NjdqOZBg7lp19hvhvVo6YTkNCwL3G2lTJIY4NnWSYWX0SJZCO7XYOO8QL6vZAZCHKV0ZAUgZBMQHfjfTYADXRMK3pKpimWy5tv1Pc5Ea' 
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=<ACCESS_TOKEN>' 
     response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
     print(status.json())
